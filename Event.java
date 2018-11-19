3c05046c2c5ffbe19ea8fca3239bf70a9d84fa0f
dorichan <ars-longa-vita@mail.ru>
2018-11-19 11:04:51 +0500

    Класс Event.

--- Event.java
+++ Event.java
@@ -1,30 +1,63 @@
+import java.util.Date;
+
 public class Event {
     public String What;
     public String Who;
+    public Date When;
     public int HowMuch;
+    private Bot Bot;
 
     public boolean isEmpty() {
-        return What == null || Who == null;
+        return What == null || Who == null || When == null;
     }
     Event() {
         What = null;
         Who = null;
         HowMuch = 0;
     }
-
-    Event(String What, String When) {
-        this.What = What;
-        this.Who = When;
+    public Event(String what, String who, Date when, int howMuch, Bot bot) {
+        super();
+        What = what;
+        Who = who;
+        When = when;
+        Bot = bot;
+        HowMuch = howMuch;
+    }
+    Event(String what, String who, Bot bot, Date when) {
+        this.What = what;
+        this.Who = who;
+        When = when;
+        Bot = bot;
+        HowMuch = getConst(what);
+    }
+    void apply()  {
+        if (!isEmpty()) {
+            System.out.println(Who + " || " + "Повышено: " + What + " на " + HowMuch);
+            Bot.getTamagochyMap().get(Who).Up(What, HowMuch);
+        }
 
     }
-    Event(String What, String When, int HowMuch) {
-        this.What = What;
-        this.Who = When;
-        this.HowMuch = HowMuch;
-    }
-    public void printEvent() {
-        System.out.println(What);
-        System.out.println(Who);
-        System.out.println(HowMuch);
+
+    int getConst(String what) {
+        switch (what) {
+            case "Питание": {
+                return 20;
+            }
+            case "Сон": {
+                return 100;
+            }
+            case "Туалет": {
+                return 50;
+            }
+            case "Счастье": {
+                return 10;
+            }
+            case "Гигиена": {
+                return 15;
+            }
+            default:
+                break;
+        }
+        return 0;
     }
 }
