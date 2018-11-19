27d2a4a6c5f06f3002c0af9e8b9ad6ed2f7d3876
dorichan <ars-longa-vita@mail.ru>
2018-11-19 11:03:19 +0500

    Событие, отвечающее за смерть

--- Death.java
+++ Death.java
@@ -1,61 +1,29 @@
-import java.util.Map;
+import java.util.Date;
 
-public class Death implements Runnable {
-    //    static void changeState (Map< String, Integer > dict) {
-//        if (dict.get("Питание") - 1 < 30) {
-//            System.out.println("Я очень голоден!!");
-//        }
-//        if (dict.get("Питание") - 1 <= 0) {
-//            System.out.println("Питание: 0. Кажется, вот он... конец...");
-//            System.exit(0);
-//        }
-//        if (dict.get("Естественная нужда") - 5 < 20) {
-//            System.out.println("Зов природы становится невыносимым! Ну отпусти на минуточку!");
-//        }
-//        if (dict.get("Естественная нужда") - 5 <= 0) {
-//            System.out.println("Тут такое дело... В общем, произошла утечка...");
-//            dict.put("Естественная нужда", 100);
-//            dict.put("Гигиена", 0);
-//        }
-//        else{
-//            dict.put("Естественная нужда", dict.get("Естественная нужда") - 5);
-//        }
-//
-//        if (dict.get("Гигиена") - 3 < 20 && dict.get("Гигиена") - 3 > 0) {
-//            System.out.println("Я, конечно, понимаю, что многие считают, мол, зачем мыться, когда есть духи" +
-//                    "и дезодоранты, но я-то не такой...");
-//        }
-//        if (dict.get("Гигиена") - 3 <= 0) {
-//            System.out.println("В общем, если хочешь подойти, обняться там... Лучше подходи в противогазе");
-//        }
-//        else {
-//            dict.put("Гигиена", dict.get("Гигиена") - 3);
-//        }
-//        if (dict.get("Сон") - 1 <= 0) {
-//            System.out.println("Кажется, я засыпаю...");
-//            try {
-//                Thread.sleep(5000);
-//                System.out.println("Ой, извини, что заснул...");
-//
-//            } catch (InterruptedException e) {
-//            }
-//            dict.put("Сон", 40);
-//
-//        }
-//        dict.put("Питание", dict.get("Питание") - 2);
-//        dict.put("Общение", dict.get("Общение") - 2);
-//        dict.put("Сон", dict.get("Сон") - 1);
-//        dict.put("Досуг", dict.get("Досуг") - 4);
-//
-//    }
-    public void run() {
-        while (true) {
-            try {
-                Event death = new Event("Death", "10", 5);
-                Bot.events.add(death);
-                Thread.sleep(5000); //1000 - 1 сек
-            } catch (InterruptedException ex) {
-            }
-        }
+public class Death extends Event {
+    private Bot bot;
+    private int coef;
+
+
+    public Death(Bot bot, String id, int coef) {
+        super();
+        this.bot = bot;
+        this.Who = id;
+        this.coef = coef;
+        this.When = new Date();
+        this.What = "Death";
+    }
+
+    void apply() {
+        System.out.println(Who + " || " + "Смерть");
+        Pet need = bot.getTamagochyMap().get(Who);
+        need.Down("Питание", 3 * coef);
+        need.Down("Сон", 2 * coef);
+        need.Down("Гигиена", 5 * coef);
+        need.Down("Счастье", 3 * coef);
+        need.Down("Туалет", 6 * coef);
+        need.setLastUpdate(new Date());
+        bot.setTamagochyMap(Who, need);
+
     }
 }
